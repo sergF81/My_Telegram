dcf24eba8c368c9d84065fd7a3cd8804d6ab06bd
sergF81 <42266878+sergF81@users.noreply.github.com>
2019-06-21 15:31:26 +0500

    Add files via upload

--- Ctelegram/src/Client.java
+++ Ctelegram/src/Client.java
@@ -6,22 +6,23 @@ public class Client {
 
     public static void main(String[] args) throws IOException {
 
-        while (true) {
-            Socket client = new Socket("127.0.0.1", 8000);
 
+        Socket client = new Socket("127.0.0.1", 8000);
+        BufferedReader nikreader = new BufferedReader(new InputStreamReader(System.in));
+        String Nikreader = nikreader.readLine();
+        while (true) {
             BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
             String Sreader = reader.readLine();
-            System.out.println();
 
             BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
-            //String Swriter = reader.readLine();
-
-            writer.write(Sreader);
+            writer.write(Nikreader + ": " + Sreader);
             writer.newLine();
             writer.flush();
-            System.out.println(Sreader);
+
+               BufferedReader sms = new BufferedReader(new InputStreamReader(client.getInputStream()));
+                String smm = sms.readLine();
+               System.out.println(smm);
         }
 
     }
-
 }
