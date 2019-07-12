804f3f20578316dcb27a1e5ffbf35737719f3ae4
sergF81 <42266878+sergF81@users.noreply.github.com>
2019-07-12 10:24:38 +0500

    Add files via upload

--- Stelegram/src/Main.java
+++ Stelegram/src/Main.java
@@ -5,7 +5,6 @@ public class Main {
     public static void main(String[] args) throws IOException {
         Server server = new Server();
         Thread serverThread = new Thread(server);
-      //  serverThread.setDaemon(true); // Теперь этот поток будет завершаться при закрытии главного окна
         serverThread.start();
 
     }
--- Stelegram/src/Server.java
+++ Stelegram/src/Server.java
@@ -1,19 +1,14 @@
-
-
 import java.io.*;
 import java.net.*;
 import java.util.ArrayList;
 
-
-public class Server implements Runnable
-{
-
+public class Server implements Runnable {
 
     ServerSocket serverSocket;
-ArrayList <Socket> user = new ArrayList<Socket>();
+    ArrayList<Socket> user = new ArrayList<Socket>();
+
     {
-        try
-        {
+        try {
             serverSocket = new ServerSocket(8000);
         } catch (IOException e) {
             e.printStackTrace();
@@ -21,66 +16,75 @@ ArrayList <Socket> user = new ArrayList<Socket>();
         }
     }
 
-
     @Override
-    public void run()
-    {
+    public void run() {
 
         System.out.println("Hi 1");
-        try
-        {
-            while (true)
-            {
+        try {
+            while (true) {
                 Client client = new Client(serverSocket.accept());
                 Thread clientThread = new Thread(client);
                 clientThread.start();
             }
-        } catch (IOException e)
-        {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
     public class Client implements Runnable {
-     public Socket clientSocket;
+        public Socket clientSocket;
 
         public Client(Socket clientSocket) throws IOException {
 
             System.out.println(clientSocket);
             this.clientSocket = clientSocket;
             user.add(this.clientSocket);
+
+            for (int i = 0; i < user.size(); i++) {
+
+                System.out.println(user.get(i) + ": " + user.size());
+                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(user.get(i).getOutputStream(), "Cp1251"));
+                writer.write("Клиент: " + clientSocket + "присоединился к чату");
+                writer.newLine();
+                writer.flush();
+            }
         }
 
         @Override
-        public void run()
-        {
-            try
-            {
+        public void run() {
+            try {
 
-                while (true)
-                {
+                while (true) {
 
-                    BufferedReader readerS = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                    BufferedReader readerS = new BufferedReader(new InputStreamReader(clientSocket.getInputStream(), "Cp1251"));
                     String Sreader1 = readerS.readLine();
                     System.out.println(Sreader1);
 
-                    for (int i=0; i< user.size(); i++)
-                    {
+                    for (int i = 0; i < user.size(); i++) {
+
                         System.out.println(user.get(i) + ": " + user.size());
-                        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(user.get(i).getOutputStream()));
-
-
-                        writer.write(user.get(i) + ": " + Sreader1);
+                        // System.out.println(user.size());
+                        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(user.get(i).getOutputStream(), "Cp1251"));
+                        writer.write(Sreader1);
                         writer.newLine();
                         writer.flush();
-                        System.out.println(user.get(i) + ": " + Sreader1);
+                        System.out.println(Sreader1);
 
                     }
 
                 }
-            } catch (IOException e)
-                 {
-                 }
+            } catch (IOException e) {
+                try {
+                    for (int i = 0; i < user.size(); i++) {
+                        if (user.get(i) == clientSocket) user.remove(i);
+                        System.out.println("Клиент: " + clientSocket + "вышел из чата.");
+                    }
+                    clientSocket.close();
+
+                } catch (IOException e1) {
+
+                }
+            }
         }
 
     }
