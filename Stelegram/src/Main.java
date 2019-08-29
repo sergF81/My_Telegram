85c18eb235616e0d7db106064a6f677dbcef0d04
sergF81 <42266878+sergF81@users.noreply.github.com>
2019-08-29 14:43:35 +0500

    Add files via upload

--- Stelegram/src/Main.java
+++ Stelegram/src/Main.java
@@ -1,13 +1,17 @@
 import java.io.IOException;
 
+/*
+ * @author fortis@tochka.com
+ * @created 27.08.19
+
+ Данный класс реализует запуск сервера чата
+*/
+
 public class Main {
 
     public static void main(String[] args) throws IOException {
-
-            Server server = new Server();
-
-      Thread serverThread = new Thread(server);
+        Server server = new Server();
+        Thread serverThread = new Thread(server);
         serverThread.start();
-//
     }
 }
--- Stelegram/src/Server.java
+++ Stelegram/src/Server.java
@@ -2,9 +2,19 @@ import java.io.*;
 import java.net.*;
 import java.util.ArrayList;
 
+/*
+ * @author fortis@tochka.com
+ * @created 27.08.19
+
+ Данный класс реализует создание соединения сервера чата
+*/
+
 public class Server implements Runnable {
+
     ServerSocket serverSocket;
+    //Массив, в которые заносятся значения clientSocket
     ArrayList<Socket> listSocked = new ArrayList<>();
+    //Массив, в которые заносятся значения nickname
     ArrayList<String> listNickname = new ArrayList<>();
 
     {
@@ -14,40 +24,24 @@ public class Server implements Runnable {
         } catch (IOException e) {
 
         }
-
     }
 
+    //метод формирует поток с созданием клиентского соединения
     @Override
     public void run() {
         while (true) {
 
-
             try {
-
                 Client client = new Client(serverSocket.accept());
-
                 Thread clientThread = new Thread(client);
-                if (clientThread.isInterrupted()) {
-                    System.out.println("Привет");
-
-
-                }
                 clientThread.start();
-
-
-
             } catch (IOException e) {
-
-
                 System.out.println("Обрыв соединения с клиентом");
-
             }
-
         }
-
-
     }
 
+    //Класс реализует обмен сообщенями между сервером и клиентом
     public class Client implements Runnable {
         public Socket clientSocket;
         private String nickname;
@@ -61,30 +55,29 @@ public class Server implements Runnable {
             listNickname.add(nickname);
 
             for (int i = 0; i < listSocked.size(); i++) {
-                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(listSocked.get(i).getOutputStream(), "Cp1251"));
-                writer.write("Клиент: " + nickname + " присоединился к чату");
-                writer.newLine();
-                writer.flush();
+                BufferedWriter writerHiMessage = new BufferedWriter(new OutputStreamWriter(listSocked.get(i).getOutputStream(), "Cp1251"));
+                writerHiMessage.write("Клиент: " + nickname + " присоединился к чату");
+                writerHiMessage.newLine();
+                writerHiMessage.flush();
             }
         }
 
+        //метод реализует обмен сообщенями между сервером и клиентом
         @Override
         public void run() {
             try {
                 while (true) {
-                    BufferedReader readerS = new BufferedReader(new InputStreamReader(clientSocket.getInputStream(), "Cp1251"));
-                    String Sreader1 = readerS.readLine();
+                    BufferedReader readerMessage = new BufferedReader(new InputStreamReader(clientSocket.getInputStream(), "Cp1251"));
+                    String Sreader1 = readerMessage.readLine();
                     System.out.println(Sreader1);
 
                     for (int i = 0; i < listSocked.size(); i++) {
-                        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(listSocked.get(i).getOutputStream(), "Cp1251"));
-                        writer.write(Sreader1);
-                        writer.newLine();
-                        writer.flush();
+                        BufferedWriter writerMessage = new BufferedWriter(new OutputStreamWriter(listSocked.get(i).getOutputStream(), "Cp1251"));
+                        writerMessage.write(Sreader1);
+                        writerMessage.newLine();
+                        writerMessage.flush();
                         System.out.println(Sreader1);
-
                     }
-
                 }
             } catch (IOException e) {
                 try {
@@ -97,18 +90,16 @@ public class Server implements Runnable {
 
                     if (listSocked.size() != 0) {
                         for (int i = 0; i < listSocked.size(); i++) {
-                            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(listSocked.get(i).getOutputStream(), "Cp1251"));
-                            writer.write("Клиент: " + nickname + " вышел из чата.");
-                            writer.newLine();
-                            writer.flush();
+                            BufferedWriter writerExitMessage = new BufferedWriter(new OutputStreamWriter(listSocked.get(i).getOutputStream(), "Cp1251"));
+                            writerExitMessage.write("Клиент: " + nickname + " вышел из чата.");
+                            writerExitMessage.newLine();
+                            writerExitMessage.flush();
                         }
                     }
-                   //
 
                 } catch (IOException e1) {
 
                 }
-
             }
             try {
                 clientSocket.close();
@@ -116,9 +107,7 @@ public class Server implements Runnable {
                 e.printStackTrace();
             }
         }
-
     }
-
 }
 
 
